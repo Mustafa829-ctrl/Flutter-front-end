3197ff843643dc19a64e02ddbb32143bc051956e
Mustafa829-ctrl <mmus33787@gmail.com>
2025-12-15 13:03:03 +0500

    Update appbar.dart

--- bin/appbar.dart
+++ bin/appbar.dart
@@ -1,1 +1,34 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
 
+class AppBarDemo extends StatelessWidget {
+  const AppBarDemo({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        leading: Icon(Icons.arrow_back_ios_new,size: 50, color: Colors.red,),
+        title: Text("App Bar Demo",style: TextStyle(
+          fontSize: 30,
+          fontWeight: FontWeight.w900,
+          color: Colors.orange
+        ),),
+        centerTitle: true,
+        backgroundColor: Colors.blue,
+        foregroundColor: Colors.white,
+        actions: [
+          Icon(Icons.settings,size: 45, color: Colors.green,),
+          Icon(Icons.notifications,size: 40, color: Colors.yellow,),
+        ],
+      ),
+      floatingActionButton: FloatingActionButton.extended(
+        backgroundColor: Colors.blue,
+          foregroundColor: Colors.white,
+          onPressed: (){}, 
+          label: Text("Add"),
+          icon: Icon(Icons.add),
+      ),
+    );
+  }
+}
